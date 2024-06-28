94090a111ef4cfc43a90ea42b6864da275e1985b
Isabelle0210 <isabelleisa646@gmail.com>
2024-06-28 15:11:29 -0300

    create sidebar

--- aulas/001/src/components/Sidebar.jsx
+++ aulas/001/src/components/Sidebar.jsx
@@ -1,9 +1,23 @@
 
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 export function Sidebar() {
     return (
-        <aside className={styles.Sidebar}>
-            sidebar
+        
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src='https://images.unsplash.com/photo-1717620380245-8e476305eb97?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'alt='Fundo'/>
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src='https://github.com/Isabelle0210.png'/>
+                <strong>Isabelle Caroline</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+                <a href='#'>
+                <PencilLine size={20}/>
+                Editar seu perfil
+                </a>
+            </footer>
         </aside>
     )
 }
