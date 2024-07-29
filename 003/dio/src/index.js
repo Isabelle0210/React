e9a0ef1de51c646c5a9eaa99c40c19dec92c3357
Isabelle0210 <isabelleisa646@gmail.com>
2024-07-29 11:58:27 -0300

    ....

--- 003/dio/src/index.js
+++ 003/dio/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/Home';
+import App from './pages/Home/index.js';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- 003/dio/src/pages/Home/index.js
+++ 003/dio/src/pages/Home/index.js
@@ -1,8 +1,13 @@
-
-export function App() {
+import {Header} from '../../components/Header';
+import Git from '../../assets/git.png';
+function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <div className='Conteudo'>
+        <img src={Git} alt='Logo do Git' className='background_logo'/>
+      </div>
     </div>
   );
 }
+export default App;
